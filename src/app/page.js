71e6405b7363dfc36ea7bee68c1b45cba902b94b
s22016 <s22016@std.it-college.ac.jp>
2023-12-01 02:58:03 +0000

    SignIn画面からSignUp画面の遷移ができた。

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,16 @@
-export default function Home() {
-	return <h1>Signin</h1>
+import s from './page.module.css'
+import SignInFrame from './SignInFrame.js'
+import Image from 'next/image'
+
+const SignIn = () => {
+        return(
+                <>
+		    <div className={s.all}>
+		    <Image src="/MUSCLE_WHITE.png" width={400} height={180} className={s.image} />
+		    <SignInFrame title="Sign In" />
+		    </div>
+		</>
+        )
 }
 
+export default SignIn
